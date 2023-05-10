b07931c88fb572e852a9a0226f1bef24737530d1
Shafeeq <shafeeqahmd87@gmail.com>
2023-05-09 23:59:26 -0400

    Knapsack files formatted

--- Week2-Knapsack/knapsack/solver.py
+++ Week2-Knapsack/knapsack/solver.py
@@ -3,14 +3,14 @@
 
 from collections import namedtuple
 
-Item = namedtuple("Item", ['index', 'value', 'weight'])
+Item = namedtuple("Item", ["index", "value", "weight"])
 
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
 
     # parse the input
-    lines = input_data.split('\n')
+    lines = input_data.split("\n")
 
     firstLine = lines[0].split()
     item_count = int(firstLine[0])
@@ -36,19 +36,21 @@ def solve_it(input_data):
             weight += item.weight
 
     # prepare the solution in the specified output format
-    output_data = str(value) + ' ' + str(0) + '\n'
-    output_data += ' '.join(map(str, taken))
+    output_data = str(value) + " " + str(0) + "\n"
+    output_data += " ".join(map(str, taken))
     return output_data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
 
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
-        with open(file_location, 'r') as input_data_file:
+        with open(file_location, "r") as input_data_file:
             input_data = input_data_file.read()
         print(solve_it(input_data))
     else:
         print(
-            'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
+            "This test requires an input file.  Please select one from the data directory. (i.e. python solver.py "
+            "./data/ks_4_0)"
+        )
